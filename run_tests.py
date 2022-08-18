4cf9cf63574efad2800e30aaad5f227578f9f7d9
Aethese <59108437+Aethese@users.noreply.github.com>
2022-08-18 15:19:01 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,15 @@ import os, sys
 os.system('cls' if os.name == 'nt' else 'clear')
 
 from rounder import tests as rt
+from upper import tests as ut
 
 rounder_test = rt.main()
+upper_test = ut.main()
+
 if rounder_test != 0:
 	print('Rounder test(s) failed!')
 	sys.exit(1)
+
+if upper_test != 0:
+	print('Upper test(s) failed!')
+	sys.exit()
