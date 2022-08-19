a02250fd9659c1768fe5018c69be5a27141a4185
Aethese <59108437+Aethese@users.noreply.github.com>
2022-08-19 08:10:41 -0500

    add option to make all characters lowercase

--- upper/upper.py
+++ upper/upper.py
@@ -1,17 +1,16 @@
 '''
-upper will make all characters in the alphabet capitalized
+capitalizes all lowercase characters in a string
 '''
 
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 
 uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lowercase = 'abcdefghijklmnopqrstuvwxyz'
 
+
 def make_upper(upper_string: str):
 	'''
-	how it works: it will go through all of the characters in the string. if the character
-	the for loop goes through is in the `lowercase` var, meaning its a lowercase character.
-	then after its figured it out that's a 
+	capitalizes all lowercase characters in a string
 	'''
 
 	if not isinstance(upper_string, str):
@@ -30,3 +29,27 @@ def make_upper(upper_string: str):
 		string_location += 1
 
 	return upper_string
+
+
+def make_lower(lower_string: str):
+	'''
+	makes all uppercase characters in a string lowercase.
+	also added just for fun c:
+	'''
+
+	if not isinstance(lower_string, str):
+		raise ValueError(f'A string is needed, not {type(lower_string).__name__}')
+
+	string_location = 0
+	for character in lower_string:
+		if character in uppercase:
+			location = 0
+			for look_in_lowercase in uppercase:
+				if look_in_lowercase == character:
+					break
+				else:
+					location += 1
+			lower_string = lower_string[:string_location] + lowercase[location] + lower_string[string_location+1:]
+		string_location += 1
+
+	return lower_string
