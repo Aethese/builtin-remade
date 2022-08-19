754ef3bf22da3082c83babe45db03c0e4ffc4527
Aethese <59108437+Aethese@users.noreply.github.com>
2022-08-19 08:11:05 -0500

    Update tests.py

--- upper/tests.py
+++ upper/tests.py
@@ -4,8 +4,9 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
 	'''
-	sorry for throwing this so quickly together
+	sorry for throwing this together so quickly
 	'''
+
 	failed = 0
 	print(f'\nRunning Upper version {upper.__version__}')
 	original = 'AbCdEakaA'
@@ -21,5 +22,12 @@ def main():
 	if not is_correct:
 		failed += 1
 	print(f'original: {original} | new: {new_upper} | new {"=" if is_correct else "!"}= correct')
+
+	original = 'ALJAkjasd(9139Ajjasd'
+	new_lower = upper.make_lower(original)
+	is_correct = True if new_lower == original.lower() else False
+	if not is_correct:
+		failed += 1
+	print(f'original: {original} | new: {new_lower} | new {"=" if is_correct else "!"}= correct')
 
 	return failed
