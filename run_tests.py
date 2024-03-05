6cf11cba11351cdb11d06e6021b574b7c80633ac
Aethese <59108437+Aethese@users.noreply.github.com>
2024-03-05 14:56:28 -0600

    don't exit on fail

--- run_tests.py
+++ run_tests.py
@@ -12,14 +12,11 @@ replace_test = ret.main()
 print(f'\n{rounder_test} rounder test(s) failed')
 if rounder_test != 0:
 	print('Rounder test(s) failed!')
-	sys.exit(1)
 
 print(f'{upper_test} upper test(s) failed')
 if upper_test != 0:
 	print('Upper test(s) failed!')
-	sys.exit(1)
 
 print(f'{replace_test} replace test(s) failed')
 if replace_test != 0:
 	print('Replace test(s) failed!')
-	sys.exit(1)
